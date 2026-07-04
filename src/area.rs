df819f8f16782dd4d7c3a7f63a0eebe8262b6e59
Jonas Engström <j.e.engstrom@gmail.com>
2026-07-04 22:23:01 +0200

    Added to_base_unit method to AreaUnit.

--- src/area.rs
+++ src/area.rs
@@ -5,17 +5,27 @@
 use crate::macros::*;
 
 use si_prefixes::Prefix;
-use crate::length::LengthUnit ;
+use crate::length::LengthUnit;
 
 pub enum AreaUnit {
     Square(Prefix, LengthUnit),
 }
 
-// impl AreaUnit {
-//     doc_to_base_unit! {
-//         fn to_
-//     }
-// }
+impl AreaUnit {
+    doc_to_base_unit! {
+        fn to_base_unit(&self) -> impl FnOnce(f64) -> f64 {
+            match self {
+                AreaUnit::Square(prefix, unit) => {
+                    let factor = unit
+                        .to_base_unit()(Prefix::conversion_constant(&prefix, &Prefix::None))
+                        .powi(2);
+
+                    move |x| factor * x
+                },
+            }
+        }
+    }
+}
 
 // impl AreaUnit {
 //     doc_to_base_unit! {
@@ -35,23 +45,23 @@ pub enum AreaUnit {
 //     }
 // }
 
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-//     #[test]
-//     fn custom_unit_to_base_unit_works_correctly() {
-//         let custom_area_unit = AreaUnit::Custom(LengthUnit::Inch, LengthUnit::Inch);
-//         let one_square_inch = custom_area_unit.to_base_unit()(1f64);
+    #[test]
+    fn custom_unit_to_base_unit_works_correctly() {
+        let square_kilometer_unit = AreaUnit::Square(Prefix::Kilo, LengthUnit::Meter);
+        let one_square_kilometer = square_kilometer_unit.to_base_unit()(1f64);
 
-//         assert_eq!(one_square_inch, 0.0254f64 * 0.0254f64);
-//     }
+        assert_eq!(one_square_kilometer, 1_000_000f64);
+    }
 
-//     #[test]
-//     fn custom_unit_from_base_unit_works_correctly() {
-//         let custom_area_unit = AreaUnit::Custom(LengthUnit::Inch, LengthUnit::Inch);
-//         let one_square_meter = custom_area_unit.from_base_unit()(1f64);
+    // #[test]
+    // fn custom_unit_from_base_unit_works_correctly() {
+    //     let custom_area_unit = AreaUnit::Custom(LengthUnit::Inch, LengthUnit::Inch);
+    //     let one_square_meter = custom_area_unit.from_base_unit()(1f64);
 
-//         assert_eq!(one_square_meter, 1f64 / (0.0254f64 * 0.0254f64));
-//     }
-// }
+    //     assert_eq!(one_square_meter, 1f64 / (0.0254f64 * 0.0254f64));
+    // }
+}
